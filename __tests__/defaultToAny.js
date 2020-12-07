347126c2731669708673754d4d2ef373c225fb02
Tommi Brusila <tommi.brusila@student.tuni.fi>
2020-12-07 16:26:56 +0200

    Fix defaultToAny tests

--- __tests__/defaultToAny.js
+++ __tests__/defaultToAny.js
@@ -1,18 +1,18 @@
-/* __tests__/defaultTo.js */
+/* __tests__/defaultToAny.js */
 
-import defaultTo from '../src/defaultTo.js';
+import defaultToAny from '../src/defaultToAny.js';
 
-describe('#defaultTo()', function() {
+describe('#defaultToAny()', function() {
   test('should return the same value when it is not "empty"', function() {
-    expect(defaultTo('lumiukko', undefined, 'juu')).toBe('lumiukko');
+    expect(defaultToAny('lumiukko', undefined, 'juu')).toBe('lumiukko');
   });
   test('should return the first allowed defaultValue when value is Nan', function() {
-    expect(defaultTo(NaN, undefined, 'default')).toBe('default');
+    expect(defaultToAny(NaN, undefined, 'default')).toBe('default');
   });
   test('should return the first allowed defaultValue when value is null', function() {
-    expect(defaultTo(null, NaN, 'default')).toBe('default');
+    expect(defaultToAny(null, NaN, 'default')).toBe('default');
   });
   test('should return the first allowed defaultValue when value is undefined', function() {
-    expect(defaultTo(undefined, null, 'default')).toBe('default');
+    expect(defaultToAny(undefined, null, 'default')).toBe('default');
   });
 });
